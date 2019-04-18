2fc5c3fc829d2c3493e83062c1d3f31074e1c503
夏卓凡 <xiazhuofan@qq.com>
2019-04-18 23:06:38 +0800

    明天再说，录入数据。

--- WordPuzzle/MainWindow.xaml.cs
+++ WordPuzzle/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Linq;
@@ -7,7 +8,9 @@ using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Threading;
+using System.ComponentModel;
 using MaterialDesignThemes.Wpf;
+
 
 namespace WordPuzzle
 {
@@ -19,11 +22,22 @@ namespace WordPuzzle
         private MainLogic backend = null;
         private DispatcherTimer delayExit = null;
         private ObservableCollection<GridPoint> scene = null;
+        private const int sceneRows = 18;
+        private const int sceneCols = 35;
+        private List<SuperChar> problems = null;
+
+        private SuperChar superCharEdt = null;
+        private bool IsEditing = false;
+        private int SelectedPuzzleIdx = -1;
 
         public MainWindow()
         {
             InitializeComponent();
             backend = MainLogic.GetInstance();
+            // Data Init.
+            backend.LoadData(".\\THUOCL_poem.txt", ".\\LUT.txt");
+            backend.LoadBank(".\\BANK.txt");
+            problems = backend.problemBank;
         }
 
         private void Backbone_Loaded(object sender, RoutedEventArgs e)
@@ -42,15 +56,15 @@ namespace WordPuzzle
             Stroke[] result = backend.GetResult();
             string[] resultInChar = backend.Translate(result[0]);
             Console.WriteLine(b);*/
+
+            // UI Init.
             CreateScene();
             lbFunctions.SelectedIndex = 0;
-            // Util.SaveProblemBank(backend.problemBank, "BANK.txt");
-            backend.LoadData(".\\THUOCL_poem.txt", ".\\LUT.txt");
-            backend.LoadBank(".\\BANK.txt");
         }
 
         private void LbiQuit_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            // Set a timer to make visual effects.
             delayExit = new DispatcherTimer();
             delayExit.Tick += new EventHandler(DelayShutdown);
             delayExit.Interval = TimeSpan.FromMilliseconds(250);
@@ -59,6 +73,7 @@ namespace WordPuzzle
 
         private void DelayShutdown(object sender, EventArgs e)
         {
+            // Call Close().
             delayExit.Stop();
             Close();
         }
@@ -71,17 +86,12 @@ namespace WordPuzzle
             }
         }
 
-        private void BtnReset_Click(object sender, RoutedEventArgs e)
-        {
-            ResetScene();
-        }
-
         private void CreateScene()
         {
             scene = new ObservableCollection<GridPoint>();
-            for(int i = 0; i < 18 * 35; ++i)
+            for(int i = 0; i < sceneCols * sceneRows; ++i)
             {
-                GridPoint gpt = new GridPoint((i % 35).ToString(), i % 35, i / 35);
+                GridPoint gpt = new GridPoint((i % sceneCols).ToString(), i % sceneCols, i / sceneCols);
                 scene.Add(gpt);
             }
             icContainer.ItemsSource = scene;
@@ -97,15 +107,32 @@ namespace WordPuzzle
 
         private void SetBenchmarkScene()
         {
-
+            foreach(SuperChar sc in problems)
+            {
+                for(int k = 0; k < sc.positions.Count; ++k)
+                {
+                    Point[] applicants = sc.positions[k];
+                    for(int l = 0;l < applicants.Length; ++l)
+                    {
+                        Point p = applicants[l];
+                        int idx = (int)p.Y * sceneCols + (int)p.X;
+                        scene[idx].SelfType = GridPoint.GridPointType.Assigned;
+                        scene[idx].AddVersePos(k, l);
+                    }
+                }
+            }
         }
 
-        private void Benchmark_Selected(object sender, RoutedEventArgs e)
+        private void LbiBenchmark_Selected(object sender, RoutedEventArgs e)
         {
             ResetScene();
+            SetBenchmarkScene();
         }
 
-        
+        private void Backbone_Closing(object sender, CancelEventArgs e)
+        {
+            backend.SaveBank(".\\BANK.txt");
+        }
     }
 
     public class GridPoint
@@ -119,12 +146,17 @@ namespace WordPuzzle
         public string SelfContent { get; set; }
         public GridPointType SelfType { get; set; }
         public Point SelfPos { get; }
-
+        public List<Point> versePos = null;
         public GridPoint(string s, int x, int y, GridPointType gpt = GridPointType.Assigned)
         {
             SelfContent = s;
             SelfPos = new Point(x, y);
             SelfType = gpt;
+            versePos = new List<Point>();
+        }
+        public void AddVersePos(int verseIdx, int wordIdx)
+        {
+            versePos.Add(new Point(verseIdx, wordIdx));
         }
     }
 
